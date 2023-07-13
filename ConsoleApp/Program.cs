685f64b4f7735da5a4ceb179218cdfee855009f5
petrbinek <binekpeter98@gmail.com>
2023-07-13 16:01:06 +0300

    Убрали слово братан и добавили обращение с ожиданием ответа

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello there boy, World!");
-        Console.ReadLine();
-        Console.WriteLine("Salam, brotan");
+       
+        Console.WriteLine("Hello there boy, World! \n Whats ur name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Salam, {0}", name);
     }
 }
 // Вообще то пишется "bratan"
